5f95c6b46d5b3ce97322a392ac17cc29e7f8da08
Saw3rt <asy7000kmmr@gmail.com>
2022-09-30 00:29:01 +0500

    AFASf

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,47 +13,33 @@ public class Calculator {
         System.out.println(Calc(exp));
     }
 
-
     public static String Calc(String exp) {
-        String[] calc_exp = exp.split("");
-        if(calc_exp.length !=3){
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Неверный формат");
-            exp =  scanner.nextLine();
-
+        String[] data = exp.split(" ");
+        if (data.length < 3) {
+            System.out.println("строка не является математической операцией");
+            System.exit(0);
+        } else if (data.length > 3) {
+            System.out.println("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            System.exit(0);
         }
+
         String strResult = null;
         Convert convert = new Convert();
-        String[] actions = {"+", "-", "/", "*"};
-        String[] regexActions = {"\\+", "-", "/", "\\*"};
-        int actionIndex = -1;
-        int count = 0;
-        for (int i = 0; i < actions.length; i++) {
-            if (exp.contains(actions[i])) {
-                actionIndex = i;
-                count++;
 
-            }
-        }
-        if (actionIndex == -1 || count > 1) {
-
-            System.out.println("Некорректное выражение");
-        }
-
-        String[] data = exp.split(regexActions[actionIndex]);
-
-        if (convert.isRoman(data[0]) == convert.isRoman(data[1])) {
+        if (convert.isRoman(data[0]) == convert.isRoman(data[2])) {
             int a, b;
             boolean isRoman = convert.isRoman(data[0]);
+
+
             if (isRoman) {
                 a = convert.romanToInt(data[0]);
-                b = convert.romanToInt(data[1]);
+                b = convert.romanToInt(data[2]);
             } else {
                 a = Integer.parseInt(data[0]);
-                b = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[2]);
             }
 
-            if (a < 1 || a > 10) {
+            if (a < 1 || a > 10 || b < 1 || b > 10) {
                 try {
                     throw new IOException();
                 } catch (IOException c) {
@@ -62,39 +48,42 @@ public class Calculator {
 
                 }
             }
+
             int result;
-                switch (actions[actionIndex]) {
-                    case "+":
-                        result = a + b;
-                        break;
-                    case "-":
-                        result = a - b;
-                        break;
-                    case "*":
-                        result = a * b;
-                        break;
-                    default:
-                        result = a / b;
-                        break;
-                }
-                if (isRoman) {
-                    if (result <= 0) {
-                        System.out.println("В римской системе нет отрицательных чисел");
-                    } else {
-
-
-                        strResult = convert.intToRoman(result);
-                    }
+            switch (data[1]) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                default:
+                    result = a / b;
+                    break;
+            }
+            if (isRoman) {
+                if (result <= 0) {
+                    System.out.println("в римской системе нет отрицательных чисел");
+                    System.exit(0);
                 } else {
-                    strResult = String.valueOf(result);
+
+
+                    strResult = convert.intToRoman(result);
                 }
             } else {
-                System.out.println("Числы в разных форматах");
+                strResult = String.valueOf(result);
             }
-            return strResult;
-
+        } else {
+            System.out.println("используются одновременно разные системы счисления");
+            System.exit(0);
         }
+        return strResult;
+
     }
+}
 
 
 
